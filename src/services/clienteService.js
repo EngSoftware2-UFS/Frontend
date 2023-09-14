c4895f8247385b8621fc3ac5450f3ba66198e684
AllaphyUFS <allaphy@academico.ufs.br>
2023-09-14 12:33:20 -0300

    feat: adding example functions

--- src/services/clienteService.js
+++ src/services/clienteService.js
@@ -2,15 +2,33 @@ import * as baseApi from './api.js'
 
 let api = baseApi.getApi()
 
-const getReservaHistory = async () => {
-    var clientId = 1;
-    api.get(`/clientes/${clientId}/reservas/historico`)
+const setHeader = () => {
+    if (localStorage.token) baseApi.setAuthToken(localStorage.token);
+}
+
+const getHistoricoReserva = async () => {
+    setHeader();
+    var user = JSON.parse(localStorage.user);
+    var clientId =user?.id;
+    return api.get(`/clientes/${clientId}/reservas/historico`)
     .then(res => {
-        console.log(res);
+        return res;
     })
     .catch(err => {
-        console.log(err);
+        return err;
     })
 }
 
-export { getReservaHistory }
+const getHistoricoEmprestimo = async () => {
+    setHeader();
+    var clientId = localStorage.user ? JSON.parse(localStorage.user)?.id : null;
+    return api.get(`/clientes/${clientId}/emprestimos/historico`)
+    .then(res => {
+        return res;
+    })
+    .catch(err => {
+        return err;
+    })
+}
+
+export { getHistoricoReserva, getHistoricoEmprestimo }
