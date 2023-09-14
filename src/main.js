dcbc626a78f29541260eee02470000e09f7a70a5
AllaphyUFS <allaphy@academico.ufs.br>
2023-09-14 00:28:48 -0300

    feat: adding notification plugin

--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import './assets/tailwind.css'
 import * as api from './services/api.js'
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-default.css';
 
 loadFonts()
 
 const app = createApp(App)
 
 app.config.globalProperties.$api = api;
+app.config.globalProperties.$user = null;
 
 app.use(router)
+.use(ToastPlugin)
 .use(vuetify)
 .mount('#app')
