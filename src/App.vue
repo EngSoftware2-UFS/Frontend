3da7fb10d437fa3a78b92579e80b3c4ff131d5b7
AllaphyUFS <allaphy@academico.ufs.br>
2023-09-13 22:06:18 -0300

    feat: adding loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app class="dark:bg-slate-700 dark:text-white">
+    <Loading v-if="loading" />
     <v-main>
       <Navbar @toggle="toggle" />
       <router-view/>
@@ -9,14 +10,51 @@
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Loading from './components/Loading.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Loading
   },
   data: () => ({
-    //
+    loading: false,
+    requests: []
   }),
+  methods: {
+    mountInterceptors: function () {
+      this.$api.getApi().interceptors.request.use(config => {
+        this.requests.push(1);
+        this.loading = true;
+        return config;
+      }, error => {
+        return error;
+      });
+
+      this.$api.getApi().interceptors.response.use(config => {
+        if (this.requests.length > 1) {
+          this.requests.pop();
+        }
+        else {
+          this.requests.pop();
+          this.loading = false;
+        }
+        return config;
+      }, error => {
+        if (this.requests.length > 1) {
+          this.requests.pop();
+        }
+        else {
+          this.requests.pop();
+          this.loading = false;
+        }
+        return error;
+      })
+    }
+  },
+  mounted() {
+    this.mountInterceptors();
+  }
 }
 </script>
