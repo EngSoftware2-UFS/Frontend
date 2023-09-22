890633405d49ca559d5a58dd127f68e16c16eb62
Lucsallk <lucsallk@hotmail.com>
2023-09-22 01:31:34 -0300

    feat: sim, tá cadastrando

--- src/views/ConsultaUsuario.vue
+++ src/views/ConsultaUsuario.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container m-auto p-10 h-full d-flex flex-col">
     <div class="mb-10">
-      <v-row class="flex w-full h-fit justify-between">
-        <v-col cols="9">
+      <v-row class="flex w-full h-fit">
+        <v-col cols="8">
           <v-text-field
             class="mx-5"
             label="Nome"
@@ -13,13 +13,24 @@
           ></v-text-field>
         </v-col>
 
-        <v-col>
+        <v-col cols="2">
           <v-select
             v-model="tipoUsuario"
             label="Buscar entre"
-            :items="['Clientes', 'Funcionários']"
+            :items="['Clientes']"
             variant="solo"
           ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="2">
+          <v-btn
+            variant="tonal"
+            size="large"
+            dense
+            @click="abrirCadastrarDialog"
+            >Cadastrar</v-btn
+          >
         </v-col>
       </v-row>
     </div>
@@ -30,7 +41,7 @@
           <thead>
             <tr>
               <th
-                v-for="(header, index) in headersCliente"
+                v-for="(header, index) in headers"
                 :key="index"
                 class="text-left"
               >
@@ -39,47 +50,17 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
+            <tr v-for="(dados, index) in clientes" :key="index">
+              <td>{{ dados.nome }}</td>
+              <td>{{ dados.email }}</td>
+              <td>{{ dados.dataCadastro.split("T")[0] }}</td>
               <td>
                 <v-col cols="auto">
                   <v-btn
                     variant="plain"
                     density="comfortable"
                     icon="mdi-pencil"
-                  ></v-btn>
-                </v-col>
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
-
-        <v-table v-else>
-          <thead>
-            <tr>
-              <th
-                v-for="(header, index) in headersFuncionario"
-                :key="index"
-                class="text-left"
-              >
-                {{ header.value }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(funcionario, index) in funcionarios" :key="index">
-              <td>{{ funcionario.nome }}</td>
-              <td>{{ funcionario.email }}</td>
-              <td>{{ funcionario.tipo }}</td>
-              <td>
-                <v-col cols="auto">
-                  <v-btn
-                    variant="plain"
-                    density="comfortable"
-                    icon="mdi-pencil"
-                    @click="editarUsuario(funcionario)"
+                    @click="abrirUsuarioDialog(dados)"
                   ></v-btn>
                 </v-col>
               </td>
@@ -90,69 +71,300 @@
     </v-row>
   </div>
 
-  <v-dialog v-model="estadoDialog" width="auto" persistent>
+  <!-- pendendo componentizar -->
+  <v-dialog v-model="estadoDialog" width="1000" persistent>
     <v-card>
       <v-card-text>
-        {{ funcionarioDisplay.nome }}
+        <v-row>
+          <h1 class="text-2xl font-semibold p-2">Informaçoes do Usuário</h1>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="usuarioDisplay.nome"
+              label="Nome completo"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="usuarioDisplay.email"
+              label="Endereço de email"
+              placeholder="exemplo@email.com"
+              type="email"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="usuarioDisplay.cpf"
+              label="CPF"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="usuarioDisplay.dataCadastro"
+              label="Data de cadastro"
+              hide-details="auto"
+              variant="plain"
+              readonly
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="bloqueio"
+              label="Data de cadastro"
+              hide-details="auto"
+              variant="plain"
+              readonly
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <h1 class="text-2xl font-semibold p-2">Informaçoes do Endereço</h1>
+        </v-row>
+        <v-row>
+          <v-col cols="4"
+            ><v-text-field
+              v-model="usuarioDisplay.endereco.logradouro"
+              label="Logradouro"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="3"
+            ><v-text-field
+              v-model="usuarioDisplay.endereco.bairro"
+              label="Bairro"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="2"
+            ><v-text-field
+              v-model="usuarioDisplay.endereco.numero"
+              label="Numero"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="3"
+            ><v-text-field
+              v-model="usuarioDisplay.endereco.cidade"
+              label="Cidade"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            ><v-text-field
+              v-model="usuarioDisplay.endereco.complemento"
+              label="Complemento"
+              hide-details="auto"
+            ></v-text-field>
+          </v-col>
+        </v-row>
       </v-card-text>
-      <v-card-actions>
-        <v-btn color="primary" block @click="estadoDialog = false"
-          >Close Dialog</v-btn
+      <v-card-actions class="flex justify-end">
+        <v-btn color="#c91827" @click="estadoDialog = false">Cancelar</v-btn>
+
+        <v-btn
+          color="green"
+          variant="outlined"
+          @click="editarUsuario(usuarioDisplay)"
+          >Editar</v-btn
         >
       </v-card-actions>
     </v-card>
   </v-dialog>
+
+  <v-dialog v-model="estadoDialogCadastro" width="1000" persistent>
+    <v-card>
+      <v-card-text>
+        <v-row>
+          <h1 class="text-2xl font-semibold p-2">Informaçoes do Usuário</h1>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              label="Nome completo"
+              hide-details="auto"
+              v-model="novoCliente.nome"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="Endereço de email"
+              placeholder="exemplo@email.com"
+              type="email"
+              v-model="novoCliente.email"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              label="CPF"
+              hide-details="auto"
+              v-model="novoCliente.cpf"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="Senha"
+              hide-details="auto"
+              v-model="novoCliente.senha"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <h1 class="text-2xl font-semibold p-2">Informaçoes do Endereço</h1>
+        </v-row>
+        <v-row>
+          <v-col cols="4"
+            ><v-text-field
+              label="Endereco"
+              hide-details="auto"
+              v-model="novoCliente.endereco.logradouro"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="3"
+            ><v-text-field
+              label="Numero"
+              hide-details="auto"
+              v-model="novoCliente.endereco.numero"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="2"
+            ><v-text-field
+              label="Bairro"
+              hide-details="auto"
+              v-model="novoCliente.endereco.bairro"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="3"
+            ><v-text-field
+              label="Cidade"
+              hide-details="auto"
+              v-model="novoCliente.endereco.cidade"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            ><v-text-field
+              label="Complemento"
+              hide-details="auto"
+              v-model="novoCliente.endereco.complemento"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions class="flex justify-end">
+        <v-btn color="#c91827" @click="estadoDialog = false">Cancelar</v-btn>
+
+        <v-btn color="green" variant="outlined" @click="cadastrarUsuario"
+          >Cadastrar</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <button @click="teste">
+    clicque
+  </button>
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from "vue";
-const tipoUsuario = ref("Funcionários");
+import { ref, computed, onBeforeMount } from "vue";
+import * as atendenteService from "../services/atendenteService";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
 let estadoDialog = ref(false);
+let estadoDialogCadastro = ref(false);
+const tipoUsuario = ref("Clientes");
 
-const funcionarios = ref([
-  {
-    nome: "Lucas",
-    email: "lucsallk@hotmail.com",
-    tipo: "Bibliotecario",
-    endereco: "Aloisio Braga",
-    numero: "202",
-  },
-  {
-    nome: "Itallo",
-    email: "capivara@hotmail.com",
-    tipo: "Atendente",
-    endereco: "Outro Braga",
-    numero: "404",
-  },
-]);
+const bloqueio = computed(() => {
+  return usuarioDisplay.value.bloqueio === false ? "Bloqueado" : "Desbloqueado";
+});
 
-const funcionarioDisplay = ref({});
+const clientes = ref([]);
 
-const headersCliente = [
+const usuarioDisplay = ref({});
+
+const headers = [
   { value: "Nome" },
   { value: "Email" },
-  { value: "Situação" },
+  { value: "Data de cadastro" },
   { value: "Ações" },
 ];
 
-const headersFuncionario = [
-  { value: "Nome" },
-  { value: "Email" },
-  { value: "Tipo" },
-  { value: "Ações" },
-];
+const abrirCadastrarDialog = () => {
+  estadoDialogCadastro.value = !estadoDialogCadastro.value;
+};
 
-const editarUsuario = (funcionario) => {
-  funcionarioDisplay.value = funcionario;
+const abrirUsuarioDialog = (dados) => {
+  usuarioDisplay.value = dados;
   estadoDialog.value = !estadoDialog.value;
 };
 
+const editarUsuario = () => {
+  // Pendente endpoint para alteração.
+  console.log(usuarioDisplay.value);
+};
+
+const novoCliente = ref({
+  nome: "",
+  cpf: "",
+  email: "",
+  senha: "",
+  endereco: {
+    logradouro: "",
+    numero: "",
+    bairro: "",
+    cidade: "",
+    complemento: "",
+  },
+});
+const teste = () => {
+  console.log('teste')
+  toast("something");
+};
+const cadastrarUsuario = () => {
+  atendenteService
+    .postCliente(novoCliente.value)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  novoCliente.value = {
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    endereco: {
+      logradouro: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      complemento: "",
+    },
+  };
+  abrirCadastrarDialog();
+};
+
 onBeforeMount(() => {
-  console.log("ola");
+  atendenteService
+    .getClientes()
+    .then((res) => {
+      clientes.value = res;
+    })
+    .catch((err) => console.log(err));
 });
 </script>
-<style>
-.v-field__input {
-  color: rgb(201, 24, 39) !important;
-}
-</style>
+<style></style>
