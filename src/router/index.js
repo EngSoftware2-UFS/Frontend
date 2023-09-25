690f4ab0a08510a635acf034d198af9a0a1a360b
Lucsallk <lucsallk@hotmail.com>
2023-09-25 20:48:31 -0300

    feat: redirecionamento caso não logado

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: { requiresAuth: false },
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  }
-]
+    path: "/login",
+    name: "login",
+    component: LoginView,
+    meta: { requiresAuth: false },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach(async (to) => {
+  const authenticated =
+    localStorage.getItem("token") !== null || "" ? true : false;
+  if (to.meta.requiresAuth && authenticated === false) return "/home";
+});
+
+export default router;
