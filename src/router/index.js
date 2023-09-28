dbea8c9c4e4340b49a9c62bfb76a54df877015db
Lucsallk <lucsallk@hotmail.com>
2023-09-28 01:40:01 -0300

    fix: url da home

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import ConsultaUsuario from '../views/ConsultaUsuario.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import ConsultaUsuario from "../views/ConsultaUsuario.vue";
 
 const routes = [
   {
@@ -12,28 +11,28 @@ const routes = [
     meta: { requiresAuth: false },
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     component: LoginView,
     meta: { requiresAuth: false },
   },
   {
-    path: '/consultaUsuario',
-    name: 'consultaUsuario',
+    path: "/consultaUsuario",
+    name: "consultaUsuario",
     component: ConsultaUsuario,
     meta: { requiresAuth: true },
-  }
-]
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
-router.beforeEach(async (to) => {
+router.beforeEach((to) => {
   const authenticated =
     localStorage.getItem("token") !== null || "" ? true : false;
-  if (to.meta.requiresAuth && authenticated === false) return "/home";
+  if (to.meta.requiresAuth && authenticated === false) return "/";
 });
 
 export default router;
