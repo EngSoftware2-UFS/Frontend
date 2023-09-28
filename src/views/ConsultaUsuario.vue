990e825a5bff86dab3c6300f5745885296a2fb67
Lucsallk <lucsallk@hotmail.com>
2023-09-28 01:41:01 -0300

    feat fix: refiz um tanto

--- src/views/ConsultaUsuario.vue
+++ src/views/ConsultaUsuario.vue
@@ -35,40 +35,14 @@
       </v-row>
     </div>
 
-    <v-row>
-      <v-col>
-        <v-table v-if="tipoUsuario === 'Clientes'" elevation="24">
-          <thead>
-            <tr>
-              <th
-                v-for="(header, index) in headers"
-                :key="index"
-                class="text-left"
-              >
-                {{ header.value }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(dados, index) in clientes" :key="index">
-              <td>{{ dados.nome }}</td>
-              <td>{{ dados.email }}</td>
-              <td>{{ dados.dataCadastro.split("T")[0] }}</td>
-              <td>
-                <v-col cols="auto">
-                  <v-btn
-                    variant="plain"
-                    density="comfortable"
-                    icon="mdi-pencil"
-                    @click="abrirUsuarioDialog(dados)"
-                  ></v-btn>
-                </v-col>
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
-      </v-col>
-    </v-row>
+    <DataTable
+      :items-per-page="itemsPerPage"
+      :headers="headers"
+      :data="clientes"
+      :density="onMobileScreen ? 'compact' : 'default'"
+      @estadoDialog="_estadoDialog"
+      @dadosUsuario="dadosUsuario"
+    />
   </div>
 
   <EditDialog
@@ -80,129 +54,68 @@
     v-model="estadoDialogCadastro"
     @closeAction="estadoDialogCadastro = !estadoDialogCadastro"
   />
-  <!-- pendendo componentizar -->
-
-  <!-- <v-dialog v-model="estadoDialogCadastro" width="1000" persistent>
-    <v-card>
-      <v-card-text>
-        <v-row>
-          <h1 class="text-2xl font-semibold p-2">Informaçoes do Usuário</h1>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-text-field
-              label="Nome completo"
-              hide-details="auto"
-              v-model="novoCliente.nome"
-            ></v-text-field>
-          </v-col>
-          <v-col>
-            <v-text-field
-              label="Endereço de email"
-              placeholder="exemplo@email.com"
-              type="email"
-              v-model="novoCliente.email"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-text-field
-              label="CPF"
-              hide-details="auto"
-              v-model="novoCliente.cpf"
-            ></v-text-field>
-          </v-col>
-          <v-col>
-            <v-text-field
-              label="Senha"
-              hide-details="auto"
-              v-model="novoCliente.senha"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <h1 class="text-2xl font-semibold p-2">Informaçoes do Endereço</h1>
-        </v-row>
-        <v-row>
-          <v-col cols="4"
-            ><v-text-field
-              label="Endereco"
-              hide-details="auto"
-              v-model="novoCliente.endereco.logradouro"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="3"
-            ><v-text-field
-              label="Numero"
-              hide-details="auto"
-              v-model="novoCliente.endereco.numero"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="2"
-            ><v-text-field
-              label="Bairro"
-              hide-details="auto"
-              v-model="novoCliente.endereco.bairro"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="3"
-            ><v-text-field
-              label="Cidade"
-              hide-details="auto"
-              v-model="novoCliente.endereco.cidade"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col
-            ><v-text-field
-              label="Complemento"
-              hide-details="auto"
-              v-model="novoCliente.endereco.complemento"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-card-text>
-      <v-card-actions class="flex justify-end">
-        <v-btn color="#c91827" @click="estadoDialogCadastro = false"
-          >Cancelar</v-btn
-        >
-
-        <v-btn color="green" variant="outlined" @click="cadastrarUsuario"
-          >Cadastrar</v-btn
-        >
-      </v-card-actions>
-    </v-card>
-  </v-dialog> -->
 </template>
 
 <script setup>
+
 import { ref, onBeforeMount } from "vue";
+import { useDisplay } from "vuetify/lib/framework.mjs";
 import * as atendenteService from "../services/atendenteService";
+import DataTable from "../components/DataTable.vue";
 import EditDialog from "../components/EditDialog.vue";
 import CadastroDialog from "../components/CadastroDialog.vue";
 
 let estadoDialog = ref(false);
+const _estadoDialog = (evento) => {
+  estadoDialog.value = evento;
+};
+
 let estadoDialogCadastro = ref(false);
 
 const tipoUsuario = ref("Clientes");
 
 const clientes = ref([]);
 
-const usuarioDisplay = ref({});
-
-const headers = [
-  { value: "Nome" },
-  { value: "Email" },
-  { value: "Data de cadastro" },
-  { value: "Ações" },
-];
-
-const abrirUsuarioDialog = (dados) => {
-  usuarioDisplay.value = dados;
-  estadoDialog.value = !estadoDialog.value;
+const usuarioDisplay = ref();
+let dadosUsuario = (evento) => {
+  usuarioDisplay.value = evento.value;
 };
+// Table Values
+const itemsPerPage = 5;
+const headers = [
+  {
+    title: "Id",
+    align: "center",
+    sortable: true,
+    key: "id",
+  },
+  {
+    title: "Nome",
+    align: "center",
+    sortable: true,
+    key: "nome",
+  },
+  {
+    title: "Email",
+    align: "center",
+    sortable: true,
+    key: "email",
+  },
+  {
+    title: "Data de cadastro",
+    align: "center",
+    sortable: true,
+    key: "dataCadastro",
+  },
+  {
+    title: "Ações",
+    align: "center",
+    sortable: false,
+    key: "actions",
+  },
+];
+const breaks = useDisplay();
+const onMobileScreen = ref(breaks.smAndDown);
 
 onBeforeMount(() => {
   atendenteService
