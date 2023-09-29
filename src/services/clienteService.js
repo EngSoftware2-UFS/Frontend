d06942ea22977f4da72aea8c706e6eca32b7d401
AllaphyUFS <allaphy@academico.ufs.br>
2023-09-29 02:06:06 -0300

    feat: clients management.

--- src/services/clienteService.js
+++ src/services/clienteService.js
@@ -6,29 +6,101 @@ const setHeader = () => {
     if (localStorage.token) baseApi.setAuthToken(localStorage.token);
 }
 
-const getHistoricoReserva = async () => {
+const getCliente = async (id) => {
     setHeader();
-    var user = JSON.parse(localStorage.user);
-    var clientId =user?.id;
-    return api.get(`/clientes/${clientId}/reservas/historico`)
+    return api
+      .get(`/clientes/${id}`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        throw err;
+      });
+};
+  
+const getClientes = (name = null, cpf = null) => {
+    setHeader();
+    var queries = [];
+    if (name != null) queries.push(`name=${name}`);
+    if (cpf != null) queries.push(`cpf=${cpf}`);
+    var query = `?${queries.join("&")}`;
+
+    return api
+      .get(`/clientes${query}`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        throw err;
+      });
+};
+  
+const createCliente = (data) => {
+    setHeader();
+    return api
+      .post("/clientes", data)
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        throw err;
+      });
+};
+
+const updateCliente = (data) => {
+    setHeader();
+    return api
+      .patch(`/clientes/${data.id}`, data)
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        throw err;
+      });
+};
+
+const deleteCliente = (id) => {
+    setHeader();
+    return api
+      .delete(`/clientes/${id}`)
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        throw err;
+      });
+};
+
+const getHistoricoReserva = async (id) => {
+    setHeader();
+    return api.get(`/clientes/${id}/reservas/historico`)
     .then(res => {
         return res;
     })
     .catch(err => {
-        return err;
+        throw err;
     })
 }
 
-const getHistoricoEmprestimo = async () => {
+const getReservas = async (id) => {
     setHeader();
-    var clientId = localStorage.user ? JSON.parse(localStorage.user)?.id : null;
-    return api.get(`/clientes/${clientId}/emprestimos/historico`)
+    return api.get(`/clientes/${id}/reservas`).then(res => { return res }).catch(err => { throw err });
+}
+
+const getEmprestimos = async (id) => {
+    setHeader();
+    return api.get(`/clientes/${id}/emprestimos`).then(res => { return res }).catch(err => { throw err });
+}
+
+const getHistoricoEmprestimo = async (id) => {
+    setHeader();
+    return api.get(`/clientes/${id}/emprestimos/historico`)
     .then(res => {
         return res;
     })
     .catch(err => {
-        return err;
+        throw err;
     })
 }
 
-export { getHistoricoReserva, getHistoricoEmprestimo }
+export { getHistoricoReserva, getHistoricoEmprestimo, getCliente, getClientes, createCliente, updateCliente, deleteCliente, getReservas, getEmprestimos  }
